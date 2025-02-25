5920b35866476162fce10d3f79ef432b21a52e39
Andrei Retsikau <154879381+Retikoff@users.noreply.github.com>
2025-02-25 20:43:13 +0300

    Clouds movement
    Clouds constantly moving to the right + regions in worldoptions for code readability

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -1,13 +1,18 @@
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class Cloud : MonoBehaviour
 {
     private const float CAMERA_OFFSET_Y = 10f;
+
     void Update()
     {
         if(transform.position.y <= Camera.main.transform.position.y - CAMERA_OFFSET_Y)
         {
             Destroy(this.gameObject);
         }
+
+        this.GetComponent<Rigidbody2D>().linearVelocityX = WorldOptions.CloudsSpeed;
     }
+
 }
--- Assets/Scripts/WorldOptions.cs
+++ Assets/Scripts/WorldOptions.cs
@@ -3,9 +3,21 @@ using UnityEngine;
 
 static public class WorldOptions 
 {
+    #region PLAYER
     static public float JumpHeight = 10.0f; 
     static public float JumpDistance = 3.3f; // Just calculated during playtest
+    #endregion PLAYER
+    
+    #region GENERATION
     static public int NumberOfGeneratingScreens = 3; 
+    #endregion GENERATION
+    
+    #region CLOUDS
     static public float CloudsFrequency = 12f;
+    static public float CloudsSpeed = 0.3f;
+    #endregion CLOUDS
+    
+    #region OTHER
     static public Vector2 screenSize; // initializing in GameManager(Awake)
+    #endregion OTHER
 }
