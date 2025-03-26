8120e24ff73eec3a7af32d1ec1066bf0afadf355
Andrei Retsikau <154879381+Retikoff@users.noreply.github.com>
2025-03-26 20:14:10 +0300

    SerializeField's
    some sirializeField's for easier interaction between editor and game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,19 +2,37 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     private Generator platformGenerator, cloudGenerator, beeGenerator, birdGenerator, boostTwoGenerator, boostThreeGenerator;
-    [SerializeField] GameObject platform, cloud, bee, bird, boostTwo, boostThree;
+    [SerializeField] GameObject platform;
+    [SerializeField] int platformChanceToSpawn;
+    [SerializeField] GameObject cloud;
+    [SerializeField] int cloudChanceToSpawn; 
+    [SerializeField] GameObject bee;
+    [SerializeField] int beeChanceToSpawn; 
+    [SerializeField] int beeCount;
+    [SerializeField] GameObject bird;
+    [SerializeField] int birdChanceToSpawn;
+    [SerializeField] int birdCount;
+    [SerializeField] GameObject boostTwo;
+    [SerializeField] int boostTwoChanceToSpawn;
+    [SerializeField] int boostTwoCount;
+    [SerializeField] GameObject boostThree;
+    [SerializeField] int boostThreeChanceToSpawn;
+    [SerializeField] int boostThreeCount;
     [SerializeField] PlayerController player;
     [SerializeField] Transform startPoint;
+
     private List<Sprite> cloudSprites;
     private const string cloudResourcesPattern = "Cloud";
     private const float  GENERATION_OFFSET_Y = 7f;
     private const float DEATHZONE_OFFSET_Y = 1.5f; 
     private const float JUMP_OFFSET_Y = 0.35f;
+    
     void Awake()
     {
         WorldOptions.screenSize = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width,Screen.height));
@@ -27,22 +45,22 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {       
-        platformGenerator = Generator.CreateGenerator(platform, startPoint.position, (int)((WorldOptions.screenSize.y * 2 + 1) / WorldOptions.JumpDistance) * WorldOptions.NumberOfGeneratingScreens,WorldOptions.JumpDistance - JUMP_OFFSET_Y, 100, 1f);
+        platformGenerator = Generator.CreateGenerator(platform, startPoint.position, (int)((WorldOptions.screenSize.y * 2 + 1) / WorldOptions.JumpDistance) * WorldOptions.NumberOfGeneratingScreens,WorldOptions.JumpDistance - JUMP_OFFSET_Y, platformChanceToSpawn, 1f);
         platformGenerator.GenerateLayer();
 
-        cloudGenerator = Generator.CreateGenerator(cloud,startPoint.position, (int)((WorldOptions.screenSize.y * 2 + 1) / WorldOptions.CloudsFrequency) * (WorldOptions.NumberOfGeneratingScreens - 1),WorldOptions.CloudsFrequency, 100, 2f, true, cloudSprites);
+        cloudGenerator = Generator.CreateGenerator(cloud,startPoint.position, (int)((WorldOptions.screenSize.y * 2 + 1) / WorldOptions.CloudsFrequency) * (WorldOptions.NumberOfGeneratingScreens - 1),WorldOptions.CloudsFrequency, cloudChanceToSpawn, 2f, true, cloudSprites);
         cloudGenerator.GenerateLayer();
 
-        beeGenerator = Generator.CreateGenerator(bee, startPoint.position, 1, WorldOptions.screenSize.y, 40, WorldOptions.screenSize.y);
+        beeGenerator = Generator.CreateGenerator(bee, startPoint.position, beeCount, WorldOptions.screenSize.y, beeChanceToSpawn, WorldOptions.screenSize.y);
         beeGenerator.GenerateLayer();
 
-        birdGenerator = Generator.CreateGenerator(bird, startPoint.position, 1, WorldOptions.screenSize.y, 20, WorldOptions.screenSize.y * 2);
+        birdGenerator = Generator.CreateGenerator(bird, startPoint.position, birdCount, WorldOptions.screenSize.y, birdChanceToSpawn, WorldOptions.screenSize.y * 2);
         birdGenerator.GenerateLayer();
 
-        boostTwoGenerator = Generator.CreateGenerator(boostTwo, startPoint.position, 1, WorldOptions.screenSize.y, 35, WorldOptions.screenSize.y * 2);
+        boostTwoGenerator = Generator.CreateGenerator(boostTwo, startPoint.position, boostTwoCount, WorldOptions.screenSize.y, boostTwoChanceToSpawn, WorldOptions.screenSize.y * 2);
         boostTwoGenerator.GenerateLayer();
 
-        boostThreeGenerator = Generator.CreateGenerator(boostThree, startPoint.position, 1, WorldOptions.screenSize.y, 14, WorldOptions.screenSize.y * 5);
+        boostThreeGenerator = Generator.CreateGenerator(boostThree, startPoint.position, boostThreeCount, WorldOptions.screenSize.y, boostThreeChanceToSpawn, WorldOptions.screenSize.y * 5);
         boostThreeGenerator.GenerateLayer();
     }
 
